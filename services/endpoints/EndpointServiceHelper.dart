82646c65605c3b8b84aaa91a085c606bfd7396bf
Robbie Boyd <robbie.c.boyd@gmail.com>
2013-12-18 14:16:51 +1300

    Add EndpointServiceHlper
    Signed-off-by: Robbie Boyd <robbie.c.boyd@gmail.com>

---
+++ services/endpoints/EndpointServiceHelper.dart
@@ -0,0 +1,11 @@
+library dartlib.services.endpoints.EndpointServiceHelper;
+
+class EndpointServiceHelper
+{
+  static const String API_DOMAIN = "";
+  static String API_LOCATION = "";
+  
+  static const String API_VERSION = "";
+  
+  
+}
